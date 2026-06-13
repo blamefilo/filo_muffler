90170eaef55ac8f1ccffb8d32ff0b13a9808241a
filo <kloud.stu@gmail.com>
2026-06-14 02:21:23 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,31 +1,31 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-name "filo_muffler"
-author "filo studios."
-discord "https://discord.gg/bErPEKvRXg"
-repository "https://github.com/blamefilo/filo_muffler"
-description "Muffler Sound Swapper"
-version "1.0.1"
+name 'filo_muffler'
+author 'filo studios.'
+discord 'https://discord.gg/bErPEKvRXg'
+repository 'https://github.com/blamefilo/filo_muffler'
+description 'Muffler Sound Swapper'
+version '1.0.1'
 
 shared_scripts {
-    "@ox_lib/init.lua",
-    "shared/sh-*.lua",
+    '@ox_lib/init.lua',
+    'shared/sh-*.lua',
 }
 
 server_scripts {
-    "server/sv-*.lua"
+    'server/sv-*.lua'
 }
 
 client_scripts {
-    "client/cl-*.lua"
+    'client/cl-*.lua'
 }
 
 files {
-    "data/*",
+    'data/*',
 }
 
 dependencies {
-    "community_bridge",
+    'community_bridge',
 }
